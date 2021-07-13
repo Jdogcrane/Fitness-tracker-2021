589fcf1ddd74d80b37db19bb158ad7e05544305b
Josh <jdogcrane@gmail.com>
2021-07-12 22:46:08 -0400

    heroku

--- routes/page-routes.js
+++ routes/page-routes.js
@@ -1,11 +1,11 @@
 const path = require("path");
 
 module.exports = function(app) {
-
+// gets exercises
   app.get("/exercise", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/exercise.html"));
   });
-
+// gets stats 
   app.get("/stats", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/stats.html"));
   });
